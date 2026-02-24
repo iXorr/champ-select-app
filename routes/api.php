fe2c0407b32e7072e404efd75d33fbf598cebe1f
Igor Zabotin <igrzbtn@gmail.com>
2026-02-24 16:09:06 +0500

    readme added

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'API отключен. Используйте web-интерфейс приложения.',
-    ]);
-});
